cc6d59eacaf9b66e840ce3d5bd9d207cdba2c8b6
HENRY <113705213+Henrytos@users.noreply.github.com>
2023-12-05 08:53:32 -0300

    estudando sequelize

---
+++ bd-sql/sequelize/migrations/20231205000738-planets.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("planets", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      position: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      createAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updateAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropDatabase("planets");
+  },
+};
